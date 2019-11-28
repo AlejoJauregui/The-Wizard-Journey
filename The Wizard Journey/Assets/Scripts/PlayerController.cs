d87f9a46c9d60a2df1319f30ca3950c58f9cfa23
Alejandro Jáuregui <alejojs9409@hotmail.com>
2019-11-28 18:30:02 -0500

    This Commit include the movement script and the first scenary to test the game.

---
+++ The Wizard Journey/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+
+    const string horizontal = "Horizontal";
+    const string vertical = "Vertical";
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {  
+        CheckButtonMovements();
+    }
+
+    void CheckButtonMovements()
+    {
+        //Desplazamiento: Espacio = Velocidad * Tiempo
+        //Horizontal movement
+        if(Mathf.Abs(Input.GetAxisRaw(horizontal)) > 0.5f)
+            this.transform.Translate(new Vector3(Input.GetAxisRaw(horizontal)* speed * Time.deltaTime,0,0));
+        //Vertical Movement
+        if(Mathf.Abs(Input.GetAxisRaw(vertical)) > 0.5f)
+            this.transform.Translate(new Vector3(0,Input.GetAxisRaw(vertical) * speed * Time.deltaTime,0));
+    }
+}
