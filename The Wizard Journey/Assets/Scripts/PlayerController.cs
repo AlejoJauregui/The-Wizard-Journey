91c44a75816813e4509ebbbfaa5b4acb62dea24c
Alejandro Jáuregui <alejojs9409@hotmail.com>
2019-11-28 23:19:46 -0500

    Second Commit: We implement the blend trees for the basic animation of our principal character. Creating the PlayerController Script and making changes with the basics variables for the movements.

--- The Wizard Journey/Assets/Scripts/PlayerController.cs
+++ The Wizard Journey/Assets/Scripts/PlayerController.cs
@@ -6,13 +6,19 @@ public class PlayerController : MonoBehaviour
 {
     public float speed;
 
+    bool walking = false;
+    Vector2 lastMovement = Vector2.zero;
     const string horizontal = "Horizontal";
     const string vertical = "Vertical";
+    const string lastHorizontal = "LastHorizontal";
+    const string lastVertical = "LastVertical";
+    const string walkingState = "Walking";
+    Animator animator;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -25,10 +31,26 @@ public class PlayerController : MonoBehaviour
     {
         //Desplazamiento: Espacio = Velocidad * Tiempo
         //Horizontal movement
+        walking = false;
         if(Mathf.Abs(Input.GetAxisRaw(horizontal)) > 0.5f)
+        {
             this.transform.Translate(new Vector3(Input.GetAxisRaw(horizontal)* speed * Time.deltaTime,0,0));
+            walking = true;
+            lastMovement = new Vector2(Input.GetAxisRaw(horizontal),0);
+        }
         //Vertical Movement
         if(Mathf.Abs(Input.GetAxisRaw(vertical)) > 0.5f)
+        {
             this.transform.Translate(new Vector3(0,Input.GetAxisRaw(vertical) * speed * Time.deltaTime,0));
+            walking = true; 
+            lastMovement = new Vector2(0,Input.GetAxisRaw(vertical));
+        }
+
+        animator.SetFloat(horizontal,Input.GetAxisRaw(horizontal));
+        animator.SetFloat(vertical,Input.GetAxisRaw(vertical));
+        animator.SetBool(walkingState,walking);
+        animator.SetFloat(lastHorizontal, lastMovement.x);
+        animator.SetFloat(lastVertical, lastMovement.y);
+
     }
 }
