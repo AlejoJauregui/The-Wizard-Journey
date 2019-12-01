229459fd12bb40be4896f12244bcc5a181dd604e
Alejandro Jáuregui <alejojs9409@hotmail.com>
2019-12-01 12:30:36 -0500

    Third Commit: II implement the second scenary, the camera movements, the enemies sprites and begin with the last scenary, the las level , the boss level.

---
+++ The Wizard Journey/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField]
+    GameObject targetToFollow;
+    [SerializeField]
+    Vector3 targetPosition;
+    //PlayerController playerSpeed;
+    [SerializeField]
+    float cameraSpeed;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FocusTheCamera();
+    }
+    void FocusTheCamera()
+    {
+        targetPosition = new Vector3 (targetToFollow.transform.position.x,targetToFollow.transform.position.y,this.transform.position.z);
+        this.transform.position = Vector3.Lerp(this.transform.position,targetPosition,cameraSpeed*Time.deltaTime);
+    }
+}
--- The Wizard Journey/Assets/Scripts/PlayerController.cs
+++ The Wizard Journey/Assets/Scripts/PlayerController.cs
@@ -14,11 +14,12 @@ public class PlayerController : MonoBehaviour
     const string lastVertical = "LastVertical";
     const string walkingState = "Walking";
     Animator animator;
-    
+    Rigidbody2D playerRigidBody;
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        playerRigidBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -34,16 +35,22 @@ public class PlayerController : MonoBehaviour
         walking = false;
         if(Mathf.Abs(Input.GetAxisRaw(horizontal)) > 0.5f)
         {
-            this.transform.Translate(new Vector3(Input.GetAxisRaw(horizontal)* speed * Time.deltaTime,0,0));
+            //this.transform.Translate(new Vector3(Input.GetAxisRaw(horizontal)* speed * Time.deltaTime,0,0));
+            playerRigidBody.velocity = new Vector2(Input.GetAxisRaw(horizontal) * speed * Time.deltaTime,playerRigidBody.velocity.y);
             walking = true;
             lastMovement = new Vector2(Input.GetAxisRaw(horizontal),0);
         }
         //Vertical Movement
         if(Mathf.Abs(Input.GetAxisRaw(vertical)) > 0.5f)
         {
-            this.transform.Translate(new Vector3(0,Input.GetAxisRaw(vertical) * speed * Time.deltaTime,0));
+            //this.transform.Translate(new Vector3(0,Input.GetAxisRaw(vertical) * speed * Time.deltaTime,0));
+            playerRigidBody.velocity = new Vector2(playerRigidBody.velocity.x, Input.GetAxisRaw(vertical) * speed * Time.deltaTime);
             walking = true; 
             lastMovement = new Vector2(0,Input.GetAxisRaw(vertical));
+        }
+        if(!walking)
+        {
+            playerRigidBody.velocity = Vector2.zero;
         }
 
         animator.SetFloat(horizontal,Input.GetAxisRaw(horizontal));
