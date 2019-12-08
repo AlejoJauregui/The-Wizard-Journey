f1cbfcfeacaa3bc75d0e4c17ccf0462894ecb884
Alejandro Jáuregui <alejojs9409@hotmail.com>
2019-12-08 15:07:32 -0500

    Sixth Commit: We develop the movement of the enemies with a basic AI for the random movements of each enemy on the scene.

---
+++ The Wizard Journey/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField]
+    float enemySpeed = 1;
+    Rigidbody2D enemyRigidBody;
+    bool isMoving;
+
+    public float timeBetweenSteps;
+    float timeBetweenStepsCounter;
+
+    public float timeToMakeStep;
+    float  timeToMakeStepCounter;
+
+    public Vector2 dicrectionToMakeStep;
+    Animator enemyAnimator; 
+    const string horizontal = "Horizontal";
+    const string vertical = "Vertical";
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRigidBody = GetComponent<Rigidbody2D>();
+        enemyAnimator = GetComponent<Animator>();
+
+        timeBetweenStepsCounter = timeBetweenSteps * Random.Range(0.5f,1.5f);
+        timeToMakeStepCounter = timeToMakeStep * Random.Range(0.5f,1.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveTheEnemy();
+    }
+
+    void MoveTheEnemy()
+    {
+        if(isMoving)
+        {
+            timeToMakeStepCounter -= Time.deltaTime;
+            enemyRigidBody.velocity = dicrectionToMakeStep;
+            if(timeToMakeStepCounter < 0)
+            {
+                isMoving = false;
+                timeBetweenStepsCounter = timeBetweenSteps;
+                enemyRigidBody.velocity = Vector2.zero; 
+            }
+        }
+        else
+        {
+            timeBetweenStepsCounter -= Time.deltaTime;
+            if(timeBetweenStepsCounter < 0)
+            {
+                isMoving = true;
+                timeToMakeStepCounter = timeToMakeStep;
+                dicrectionToMakeStep = new Vector2(Random.Range(-1,2),Random.Range(-1,2)) * enemySpeed;
+            }
+        }
+        enemyAnimator.SetFloat(horizontal,dicrectionToMakeStep.x);
+        enemyAnimator.SetFloat(vertical, dicrectionToMakeStep.y);
+    }
+}
