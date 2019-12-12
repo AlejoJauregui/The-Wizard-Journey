b02e70dd1dd06c0de5ce492064676745597f9265
Alejandro Jáuregui <alejojs9409@hotmail.com>
2019-12-12 06:05:08 -0500

    Revert "Revert "Eleventh Commit: Fix attack bug and level character implementation.""
    This reverts commit 1e216120f40da746ca11a00c8aa079c1cff24085.

--- The Wizard Journey/Assets/Scripts/CharacterStats.cs
+++ The Wizard Journey/Assets/Scripts/CharacterStats.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class CharacterStats : MonoBehaviour
 {
+    public int currentLevel = 0;
+    public int currentExp = 0;
+    public int [] expToLevelUp;
+
+    public int [] hpLevels, strengthLevels, defendsLevels;
+
+    private HealthManager healthManager;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        healthManager = GetComponent<HealthManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        checkLevelPlayer();
+    }
+
+    void checkLevelPlayer()
+    {
+        if(currentLevel >= expToLevelUp.Length)
+            return;
+        if(currentExp >= expToLevelUp[currentLevel])
+        {
+            currentLevel++;
+            healthManager.UpdateMaxHealth(hpLevels[currentLevel]);
+        }
+    }
+
+    public void AddExperience(int experience)
+    {
+        currentExp += experience;
     }
 }
